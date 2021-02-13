78389f9dfad6fd0d02eb77804ee36697df9ccc1a
prajotsurey <prajotsurey@gmail.com>
2021-02-13 16:52:48 +0530

    margin

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -5,7 +5,8 @@ const Notification = () => {
   const style = {
     border: 'solid',
     padding: 10,
-    borderWidth: 1
+    borderWidth: 1,
+    marginBottom:10
   }
   if(notification){
     return (
